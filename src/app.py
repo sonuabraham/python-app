a6e00e6c1636d6b72261010183cfc287a1d46641
--replace-all <workshop-user@example.com>
2026-04-14 21:05:24 +1000

    Initial commit.

---
+++ src/app.py
@@ -0,0 +1,25 @@
+
+'/aop/v1/healthz'
+
+
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def details():
+    return jsonify({
+        'hostname': socket.gethostname(),
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p on  %B %d , %Y")
+    }
+    )
+@app.route('/api/v1/healthz')
+def health():
+     return jsonify({
+        'status': "up"
+    }), 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
